63408efd5286ef74e0eb6aade0c1686da86c98ab
Adair Juneo <adair_juneo@hotmail.com>
2021-11-28 16:42:20 -0300

    Config MongoDB e app disponivel na Heroku.

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 
@@ -9,7 +13,16 @@ app.set('views', __dirname + '/views')
 
 app.use(express.static('public'))
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL,{
+    useNewUrlParser: true
+})
+
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Conectado ao banco de dados'))
+
 app.use('/', fileRouteIndex)
 app.use('/', fileRouteFinances)
 
-app.listen('3000' , () => {console.log('Server iniciado na porta 3000')})
+app.listen(process.env.PORT || 3000, () => {console.log('Server iniciado na porta 3000')})
