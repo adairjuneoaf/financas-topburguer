010f3350db175e98eaa3f752bde53e03154078a9
Adair Juneo <adair_juneo@hotmail.com>
2021-11-30 23:56:16 -0300

    Bug fix upload ambient production .

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,4 @@
 const express = require('express')
 const routes = express.Router()
 
-const passport = require('passport')
-const localStrategy = require('passport-local').Strategy
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-
 module.exports = routes
